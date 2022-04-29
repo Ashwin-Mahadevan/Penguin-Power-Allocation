c4dfcfb46c8d90c0848a73c3e996a28e6d1d666f
Ashwin Mahadevan <ashwin.devan01@gmail.com>
2022-04-28 17:41:46 -0700

    First implementation of greedy solution

--- greedy.py
+++ greedy.py
@@ -1,8 +1,7 @@
-
 def create_covermap(D, Rs, cities):
     """ Precomputes the set of cities that would be covered by placing a tower at (x, y). Specifically, returns a dictionary mapping each potential tower location (x, y) to a set containing the coordinates of the cities that such a tower would cover. """
 
-    covered_by = dict()
+    covers = dict()
 
     for x in range(D):
         for y in range(D):
@@ -15,6 +14,27 @@ def create_covermap(D, Rs, cities):
                 if (x - X)**2 + (y - Y)**2 <= Rs**2:
                     covered.add(city)
 
-            covered_by[(x, y)] = covered
-    
-    return covered_by
+            covers[(x, y)] = covered
+
+    return covers
+
+
+def solve_greedy(D, Rs, Rp, cities):
+
+    covers = create_covermap(D, Rs, cities)
+
+    uncovered = set(cities)
+
+    towers = list()
+
+    def uncovered_size(c):
+        covers[c].intersection_update(uncovered)
+        return len(covers[c])
+
+    while uncovered:
+
+        best_tower = max(covers, key=uncovered_size)
+        towers.append(best_tower)
+        uncovered.difference_update(covers[best_tower])
+
+    return towers
